82d2a1f804c5b1a33039d376f59d8578a045ba45
wzl <41833203@qq.com>
2020-12-07 07:28:44 +0800

    Initial commit

---
+++ src/main/java/cn/edu/bupt/todolist_4/entity/Todo.java
@@ -0,0 +1,17 @@
+package cn.edu.bupt.todolist_4.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+}
